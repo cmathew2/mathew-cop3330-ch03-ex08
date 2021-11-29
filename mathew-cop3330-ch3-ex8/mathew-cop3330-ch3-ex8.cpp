9874022b0cb1f7ae3deeec847817f73065cbfc4d
Chris Mathew <cmathew2@Knights.ucf.edu>
2021-11-28 19:44:16 -0500

    edit

--- mathew-cop3330-ch3-ex8/mathew-cop3330-ch3-ex8.cpp
+++ mathew-cop3330-ch3-ex8/mathew-cop3330-ch3-ex8.cpp
@@ -9,9 +9,9 @@ int main()
     cin >> num;
 
     if (num % 2 == 0)
-        cout << "The value " << num << "is an even number\n";
+        cout << "The value " << num << " is an even number\n";
     else 
-        cout << "The value " << num << "is an odd number\n";
+        cout << "The value " << num << " is an odd number\n";
 
     return 0;
 }
